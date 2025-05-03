f15f0e530b9a50324b860ac270c2a41f80f15b24
ali ostadi <ali.ostadiy@gmail.com>
2025-05-04 01:20:37 +0200

    add if condition to check bucket existance

--- send-to-object-storage.py
+++ send-to-object-storage.py
@@ -15,15 +15,27 @@ def upload_files_from_directory(directory):
     try:
         s3_resource = boto3.resource(
             's3',
-            endpoint_url = "https://s3.ir-thr-at1.arvanstorage.ir",
-            aws_access_key_id = access_key,
-            aws_secret_access_key = secret_key
+            endpoint_url="https://s3.ir-thr-at1.arvanstorage.ir",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
         )
 
         bucket_name = 'getdatafromgithub110113'
         bucket = s3_resource.Bucket(bucket_name)
-        bucket.create(ACL='public-read')
 
+        # Check if the bucket exists
+        try:
+            s3_resource.meta.client.head_bucket(Bucket=bucket_name)
+            logging.info(f"Bucket '{bucket_name}' already exists.")
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                # Bucket does not exist, create it
+                bucket.create(ACL='public-read')
+                logging.info(f"Bucket '{bucket_name}' created successfully.")
+            else:
+                raise
+
+        # Upload files to the bucket
         for filename in os.listdir(directory):
             file_path = os.path.join(directory, filename)
             if os.path.isfile(file_path):  # Check if it's a file
@@ -41,5 +53,5 @@ def upload_files_from_directory(directory):
         logging.error(e)
 
 # Specify the directory containing files to upload
-directory_path = './stage'  # Change this to your directorygf
+directory_path = './stage'  # Change this to your directory
 upload_files_from_directory(directory_path)
